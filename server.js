a5660f2baf36532fecc2a071f48d4e6d2f2ddc2f
Dima Savkov <dimsaw85@gmail.com>
2023-02-16 15:08:23 +0200

    work with server

--- server.js
+++ server.js
@@ -1,5 +1,17 @@
-const app = require('./app')
+const app = require("./app");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const PORT = process.env.PORT || 3000;
+
+const start = async () => {
+  try {
+    app.listen(PORT, (error) => {
+      if (error) console.error("Error at server launch", error);
+      console.log(`Server running. Use our API on port: ${PORT}`);
+    });
+  } catch (error) {
+    console.error(`Failed to lauch apllication with error ${error.message}`);
+    process.exit(1);
+  }
+};
+
+start();
